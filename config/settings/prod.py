32088f7ef0c1bccb201682d068f526f4252784ca
KAE REN <hkr0528@gmail.com>
2025-09-30 12:20:39 +0800

    Fix db error

--- config/settings/prod.py
+++ config/settings/prod.py
@@ -2,3 +2,7 @@ from .base import * # noqa
 SECURE_PROXY_SSL_HEADER = ("HTTP_X_FORWARDED_PROTO", "https")
 SESSION_COOKIE_SECURE = True
 CSRF_COOKIE_SECURE = True
+
+DATABASES = {
+    "default": env.db("DATABASE_URL")
+}
